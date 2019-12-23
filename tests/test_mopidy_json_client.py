55bde7fa1882817655e4fccffc26ae6185a88665
Guillaume Vankeerberghen <gvankeerberghen@users.noreply.github.com>
2019-12-23 13:16:01 +0100

    CREATE test

--- tests/test_mopidy_json_client.py
+++ tests/test_mopidy_json_client.py
@@ -1,5 +1,6 @@
 import time
 from mopidy_json_client import MopidyClient
+from mopidy_json_client.formatting import print_nice
 
 def print_track_info(tl_track):
     track = tl_track.get('track') if tl_track else None
@@ -13,8 +14,19 @@ def print_track_info(tl_track):
     }
     print('Now playing: {artists} - {name}'.format(**trackinfo))
 
+def show_tracklist(tracklist):
+    print_nice('> Current Tracklist: ', tracklist, format='tracklist')
+
 mopidy = MopidyClient()
 mopidy.bind_event('track_playback_started', print_track_info)
+
+mopidy.tracklist.get_tl_tracks(on_result=show_tracklist)
+
+mopidy.tracklist.add(uris=['local:track:01%20Home%20Recording.m4a' ,'local:track:Zonder%20totetrekkerie.mp3'])
+
+mopidy.tracklist.get_tl_tracks(on_result=show_tracklist)
+
+mopidy.playback.play()
 
 if __name__ == '__main__':
 
